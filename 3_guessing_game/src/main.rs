7b08c4256f621cd17043f8cbf6b573e8dece5672
Nicholas Hazel <nicholas.hazel@gmail.com>
2023-06-20 21:47:04 -0700

    chore: cleaned up comments

--- 3_guessing_game/src/main.rs
+++ 3_guessing_game/src/main.rs
@@ -1,23 +1,52 @@
-// standard input output lib
+// standard input output
 use std::io;
+// random number generator
+use rand::Rng;
+// standard sorting and ordering
+use std::cmp::Ordering;
 
 fn main() {
+    // Generate a random number from 1-100
+    // This is a local seed generated with current thread and OS
+    let secret_number = rand::thread_rng()
+        .gen_range(1..=100);
+    
     println!("Guess the number!");
-    println!("Please input your guess:");
 
-    // mutable string variable
-    let mut guess = String::new();
+    // infinite loop - play forever!
+    loop {
+        println!("Please input your guess:");
+        // mutable string variable
+        let mut guess = String::new();
+    
+        // standard input - read user input
+        io::stdin()
+            // read first line of user input
+            // & indicates Ref, and single memory allocation
+            .read_line(&mut guess)
+            // Result itself has an `expect()` method for error handle
+            .expect("Failed to read line");
+    
+        // Parse input str back into u32 int, good for small, positive integers
+        let guess: u32 = match guess.trim().parse() {
+            // continue with guesses and reexecute loop
+            Ok(num) => num,
+            // on error, smother exception. Onward!
+            Err(_) => continue,
+        };
 
-    // standard input
-    io::stdin()
-        // read first line of user input
-        // & indicates Ref, and single memory allocation
-        .read_line(&mut guess)
-        // Result itself has an `expect()` method we can call
-        //
-        .expect("Failed to read line");
-
-    // use variable (also demonstrate expansion)
-    // empty {} expand args in order
-    println!("You guessed: {guess} and expanded number is: {}", 5 + 5);
+        // use variable in str
+        println!("You guessed: {guess}");
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Higher..."),
+            Ordering::Greater => println!("Lower..."),
+            // Termination condition!
+            Ordering::Equal => {
+                println!("WINNER CHICKEN DINNER THINGY");
+                // Break terminates the
+                break;
+            }
+        }
+    }
 }
